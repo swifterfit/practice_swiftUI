1df0b32f027087df428e818f1464fe8405eae1b7
Emir <swifterfit@gmail.com>
2025-01-24 14:15:24 +0800

    update new project landmarks

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 苏亮 on 23.1.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 苏亮 on 23.1.25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
